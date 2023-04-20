a67a0f66b2e83ee4990002bfbc7229e42133ae00
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-04-20 19:24:50 +0200

    skill issue
    meglio togliere tutto quello che è python (tranne il downloader)

--- Olinfo/downloader.py
+++ Olinfo/downloader.py
@@ -14,8 +14,10 @@ for i in json.loads(jsonResp)["scores"]:
     for e in json.loads(jsonRespTask)["submissions"]:
         if int(e["score"]) > max:
             max = e["score"]
-            dig = e["files"][0]["digest"]
-            name = e["files"][0]["name"]
+            for i in e["files"]:
+                if not ".py" in i["name"]:
+                    dig = e["files"][0]["digest"]
+                    name = e["files"][0]["name"]
     text = s.get(f"https://training.olinfo.it/api/files/{dig}/{name}").text.encode().replace(b"\n\n", b"\n")
     fileRead = open(os.getcwd() + "\\training\\" + name, "r")
     f = str(fileRead.readline())

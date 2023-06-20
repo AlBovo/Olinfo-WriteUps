0b9a4cd34178dde3188ce23cd6fbd31918697e7f
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-06-20 12:32:03 +0200

    problemi di alberi

---
+++ Olinfo/training/licenziamenti.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#pragma GCC optmize("O2")
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int n, tot = 0; fin >> n;
+	vector<int>v(n);
+	vector<vector<int>>f(n);
+	for(int i=0; i<n; i++){
+		fin >> v[i];
+		int b; fin >> b;
+		if(b == -1) continue;
+		f[b].push_back(i);
+	}
+	/*
+	for(int i=0; i<n; i++){
+		cout << i << ": ";
+		for(int e:f[i]){
+			cout << e << " ";
+		}
+		cout << "\n";
+	}
+	*/
+	queue<int>q;
+	q.push(0);
+	while(!q.empty()){
+		int pos = q.front();
+		q.pop();
+		for(int i:f[pos]){
+			tot += v[pos] < v[i];
+			v[i] = min(v[pos], v[i]);
+			q.push(i);
+		}
+	}
+	fout << tot;
+	return 0;
+}
---
+++ Olinfo/training/ois_olimpo.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#pragma GCC optmize("O2")
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main(){
+	int n, maxi = 0; fin >> n;
+	vector<vector<pair<int, int>>>v(n); // v[persona][0..size]{figlio, distanza}
+	for(int i=1; i<n; i++){
+		char c; int p, d; fin >> c >> p;
+		
+		if(c == 'P') d = 1;
+		else if(c == 'N') d = 2;
+		else d = 3;
+		
+		v[p].push_back({i, d});
+	}
+	queue<int>q; q.push(0);
+	vector<int>p(n, 0);
+	while(!q.empty()){
+		int pos = q.front();
+		q.pop();
+
+		for(auto i:v[pos]){
+			p[i.first] = p[pos] + i.second;
+			q.push(i.first);
+			maxi = max(maxi, p[i.first]);
+		}
+	}
+	fout << maxi;
+	return 0;
+}

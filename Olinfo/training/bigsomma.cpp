a495fd31d2183145e11ebf3a7dac7fc651c7c322
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-12-14 17:11:54 +0100

    bigsomma sol (not really that fast)

--- Olinfo/training/bigsomma.cpp
+++ Olinfo/training/bigsomma.cpp
@@ -1,49 +1,37 @@
-// Punti: 2.0
-#pragma GCC optimize("Ofast")
-#include<bits/stdc++.h>
+// Punti: 100.0
+#include<stdio.h>
+#include<unistd.h>
+// #pragma GCC optimize("O2")
 
-using namespace std;
+#define BUFFER 32768
+static char buff[BUFFER];
 
-typedef long long int ll;
-
-ll fastread(FILE *f)
-{
-    ll k = 0;
-    char c = fgetc(f);
-    bool neg = false;
+long long somma(FILE *f){
+    int fd = fileno(f), n = 0, c, r = 0;
     
-    if(c == '-')
-    {
-        neg = true;
-        c = fgetc(f);
-    }
-    
-    while(true)
-    {
-        if(c >= '0' && c <= '9')
-        {
-            k = k * 10 + (c - '0');
+    while((c = read(fd, &r, 1) != 0) && r != '\n') n = n * 10 + (r - '0');
+    // printf("%d\n", n);
+    register long long sum = 0, tot = 0;
+    short flag = 1;
+    while(c){
+        c = read(fd, buff, BUFFER);
+        for(int e=0; e<c; e++){
+            if(buff[e] < '0'){
+                sum += tot * flag;
+                tot = 0;
+                flag = -2 * (buff[e] == '-') + 1;
+            }
+            else{
+                tot = tot * 10 + (buff[e] - '0');
+            }
         }
-        else
-        {
-            break;
-        }
-        c = fgetc(f);
     }
-    
-    if(neg)
-    {
-        k *= -1;
-    }
-    
-    return k;
+    return sum;
 }
 
-long long somma(FILE *f)
-{
-    ll N = fastread(f), k = 0;
-
-    for(ll i=0; i<N; i++) k += fastread(f);
-
-    return k;
+#ifndef EVAL
+int main(){
+    printf("%lld\n", somma(stdin));
+    return 0;
 }
+#endif

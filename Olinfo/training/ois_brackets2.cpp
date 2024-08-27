fd3654fc5672cbdc17239cd03eb4359f05713e0a
Alan Davide Bovo <88632271+AlBovo@users.noreply.github.com>
2024-08-27 10:15:47 +0000

    added new solutions of DPs

---
+++ Olinfo/training/ois_brackets2.cpp
@@ -0,0 +1,42 @@
+// Punti: 100.0
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, maxsize = 50000*2;
+vector<int>A;
+vector<bool>risp;
+vector<vector<int>>dp;
+
+int solve(int pos, int sum){
+    if(pos == n){
+        if(sum == 0)
+            return dp[pos][sum] = 1;
+        return dp[pos][sum] = 0;
+    }
+    if(dp[pos][sum] != -1)
+        return dp[pos][sum];
+    risp[pos] = true;
+    if(solve(pos + 1, sum + A[pos]) == 1)
+        return dp[pos][sum] = 1;
+    risp[pos] = false;
+    if(sum - A[pos] >= 0)
+        if(solve(pos + 1, sum - A[pos]) == 1)
+            return dp[pos][sum] = 1;
+    return dp[pos][sum] = 0;
+}
+
+int main(){
+    cin >> n;
+    A.resize(n);
+    risp.resize(n);
+    dp.resize(n + 1, vector<int>(maxsize, -1));
+    for(int &i : A)
+        cin >> i;
+
+    if(solve(0, 0) == 0)
+        cout << -1;
+    else
+        for(int i=0; i<n; i++)
+            cout << string(A[i], risp[i] ? '(' : ')'); 
+    return 0;
+}
---
+++ Olinfo/training/ois_police4.cpp
@@ -0,0 +1,39 @@
+// Punti: 65.0
+#include<bits/stdc++.h>
+using namespace std;
+int n, r, t, l;
+vector<int>v;
+vector<vector<unordered_map<int, int>>>dp;
+// [v_i][r][POS]
+
+bool status(int N){
+    return (int)(N / t) % 2 == 0; // True = Green, False = Red
+}
+
+int fdp(int pos, int res, int tim){
+    if(pos == n)
+        return 0;
+    if(dp[pos][res].count(tim))
+        return dp[pos][res][tim];
+
+    int dist = ((pos == n - 1) ? l : v[pos+1]) - v[pos];
+    if(status(tim)){
+        return dp[pos][res][tim] = (fdp(pos + 1, res, tim + dist) + dist);
+    }
+    else{
+        int next = ((int)(tim / t) + 1) * t;
+        int choice = fdp(pos + 1, res, next + dist) + (next - tim);
+        if(res > 0)
+            choice = min(choice, fdp(pos + 1, res - 1, tim + dist));
+        return dp[pos][res][tim] = (choice + dist);
+    }
+}
+
+int main(){
+    cin >> n >> r >> t >> l; 
+    v.resize(n);
+    dp.resize(n, vector<unordered_map<int, int>>(r+1));
+    for(int &i : v) cin >> i;
+    cout << fdp(0, r, v[0]) + v[0];
+    return 0;
+}
